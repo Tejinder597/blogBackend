87a77382cbb0b361d7685822b2fa9d573a5053e0
Tejinder goriye <tejindergoriye@gmail.com>
2024-12-19 10:33:39 +0530

    altered_code

--- database.js
+++ database.js
@@ -8,13 +8,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // Required for some hosted databases
-      },
-    },
+    dialect: 'postgres'
   }
 )
 
