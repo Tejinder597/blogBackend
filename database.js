8edb9d3d3c9464b7673d45ab600b4025aaa3661d
Tejinder goriye <tejindergoriye@gmail.com>
2024-12-19 15:10:13 +0530

    refactored_code

--- database.js
+++ database.js
@@ -8,7 +8,8 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    port: process.env.DB_PORT || 5432,
   }
 )
 
