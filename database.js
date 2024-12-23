6ab334c06c6f6393b75d0bb078f3266f15412688
Tejinder goriye <tejindergoriye@gmail.com>
2024-12-23 15:24:40 +0530

    updated_code

--- database.js
+++ database.js
@@ -3,8 +3,8 @@ require('dotenv').config({ path: `${process.cwd()}/.env` })
 const { Sequelize } = require('sequelize')
 
 const sequelize = new Sequelize(
-  process.env.POSTGRES_NAME,
-  process.env.POSTGRES_USERNAME,
+  process.env.POSTGRES_DATABASE,
+  process.env.POSTGRES_USER,
   process.env.POSTGRES_PASSWORD,
   {
     host: process.env.POSTGRES_HOST,
