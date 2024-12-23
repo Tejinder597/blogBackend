cdbf79634ba0e8133e41fa7958cd3cfc95949300
Tejinder goriye <tejindergoriye@gmail.com>
2024-12-23 23:05:56 +0530

    updated_code

--- database.js
+++ database.js
@@ -12,7 +12,7 @@ const sequelize = new Sequelize(
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
-        require: true, 
+        require: true,
         rejectUnauthorized: false
       }
     }
